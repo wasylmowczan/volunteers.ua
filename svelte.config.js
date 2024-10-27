eda49b927e95403467deb390d36c77bf4b2e5ce9
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-10-27 18:17:10 +0100

    Remove wrangler file

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,6 @@ const config = {
 				exclude: ['<all>']
 			},
 			platformProxy: {
-				configPath: 'wrangler.toml',
 				environment: undefined,
 				experimentalJsonConfig: false,
 				persist: false
